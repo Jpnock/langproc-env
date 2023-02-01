057a10320d8d136df31417dd3d0dc90e61332dc6
James <jpnock@gmail.com>
2023-02-01 00:54:28 +0000

    test: Re-add toolchain test script

---
+++ toolchain_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Author : James Nock (@Jpnock)
+# Year   : 2023
+
+set -euo pipefail
+
+OUT="bin/driver"
+
+make bin/c_compiler
+./bin/c_compiler -S "compiler_tests/_example/example.c" -o "bin/riscv_example.s"
+riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "bin/riscv_example" "bin/riscv_example.s" "compiler_tests/_example/example_driver.c"
+
+set +e
+spike pk "bin/riscv_example"
+if [ $? -eq 0 ]; then
+    echo "Test successful"
+else
+    echo "The simulator did not exit correctly :("
+fi
+set -e
+
+rm bin/riscv_example
+rm bin/riscv_example.s
