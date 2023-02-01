42c9ceabf753948e957e5a13b62549380708490c
James <jpnock@gmail.com>
2023-02-01 00:34:05 +0000

    test: Add script to test the compiler

--- test.sh
+++ test.sh
@@ -3,14 +3,57 @@
 # Author : James Nock (@Jpnock)
 # Year   : 2023
 
-set -euo pipefail
+#!/bin/bash
 
-OUT="bin/driver"
+set -uo pipefail
+shopt -s globstar
+
+make bin/c_compiler
 
 mkdir -p bin
-/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "${OUT}" test/aux.s test/driver.c
+mkdir -p bin/output
 
-set +e
-spike pk "${OUT}"
-echo "Exited with code ${?}"
-set -e
+TOTAL=0
+PASSING=0
+
+for DRIVER in compiler_tests/**/*_driver.c; do
+    let TOTAL++
+
+    TO_ASSEMBLE="${DRIVER%_driver.c}.c"
+    LOG_PATH="${TO_ASSEMBLE//\//_}"
+    LOG_PATH="./bin/output/${LOG_PATH%.c}"
+
+    echo "${TO_ASSEMBLE}"
+
+    OUT="${LOG_PATH}"
+    rm -f "${OUT}.s"
+    rm -f "${OUT}.o"
+    rm -f "${OUT}"
+    ./bin/c_compiler -S "${TO_ASSEMBLE}" -o "${OUT}.s" 2> "${LOG_PATH}.compiler.stderr.log" > "${LOG_PATH}.compiler.stdout.log"
+    if [ $? -ne 0 ]; then
+        echo "\t> Fail: see ${LOG_PATH}.compiler.stderr.log and ${LOG_PATH}.compiler.stdout.log"
+        continue
+    fi
+
+    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "${OUT}.o" -c "${OUT}.s" 2> "${LOG_PATH}.assembler.stderr.log" > "${LOG_PATH}.assembler.stdout.log"
+    if [ $? -ne 0 ]; then
+        echo -e "\t> Fail: see ${LOG_PATH}.assembler.stderr.log and ${LOG_PATH}.assembler.stdout.log"
+        continue
+    fi
+
+    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -static -o "${OUT}" "${OUT}.o" "${DRIVER}" 2> "${LOG_PATH}.linker.stderr.log" > "${LOG_PATH}.linker.stdout.log"
+    if [ $? -ne 0 ]; then
+        echo -e "\t> Fail: see ${LOG_PATH}.linker.stderr.log and ${LOG_PATH}.linker.stdout.log"
+        continue
+    fi
+
+    spike pk "${OUT}" > "${LOG_PATH}.simulation.log"
+    if [ $? -eq 0 ]; then
+        echo -e "\t> Pass"
+        let PASSING++
+    else
+        echo -e "\t> Fail: simulation did not exit with exit code 0"
+    fi
+done
+
+printf "\nPassing %d/%d tests\n" "${PASSING}" "${TOTAL}"
